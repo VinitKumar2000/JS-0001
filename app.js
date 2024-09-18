65305aa47732097d34b5159440893d7338efdeb7
VinitKumar2000 <vinitkumar7207255570@gmail.com>
2024-09-18 19:45:47 +0530

    files ready

---
+++ app.js
@@ -0,0 +1,25 @@
+const employee={
+    calcTax(){
+        console.log("Tax rate is 10%");
+    }
+};
+
+const karanArjun={
+    salary:50000
+}
+karanArjun.__proto__=employee;
+
+
+const karanArjun2={
+    salary:60000
+}
+karanArjun2.__proto__=employee;
+
+
+const karanArjun3={
+    salary:70000,
+    calcTax(){
+        console.log("Tax rate is 30%");             //Same method in both the functions.
+    }
+}
+karanArjun3.__proto__=employee;
